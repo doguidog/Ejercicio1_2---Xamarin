be7019996b88501eb905816f80db56d3b1b4f303
Marvin Josue Martinez Zapata <101903394+doguidog@users.noreply.github.com>
2023-02-16 21:07:45 -0600

    Add project files.

---
+++ Ejercicio1_2/Ejercicio1_2/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Ejercicio1_2.Modelo;
+
+namespace Ejercicio1_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnenviar_Clicked(object sender, EventArgs e)
+        {
+            var resultado = new Persona
+            {
+                Nombre = txtnombre.Text,
+                Apellidos = txtapellidos.Text,
+                Edad = txtedad.Text,
+                Correo = txtcorreo.Text
+            };
+
+            var enviarDatos = new Page2();
+            enviarDatos.BindingContext= resultado;
+            await Navigation.PushAsync(enviarDatos);
+
+        }
+    }
+}
---
+++ Ejercicio1_2/Ejercicio1_2/Modelo/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio1_2.Modelo
+{
+    public class Persona
+    {
+
+        public string Nombre { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string Edad { get; set; }
+
+        public string Correo { get; set; }
+    }
+}
